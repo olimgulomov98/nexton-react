341680c6e5212dd8d9efcfe41ab6ed875ad5986d
olimgulomov98 <olimgulomov98@gmail.com>
2024-06-09 00:25:18 +0500

    feat: MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import '../css/app.css';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div>HOME</div>
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{my: 4}}>
+          <Typography>
+            Create React App on Typescript with REDUX
+          </Typography>
+        </Box>
+        <Button variant="contained">Contained</Button>
+      </Stack>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import "./css/index.css";
+import theme from './app/theme';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
       <App />
+      </ThemeProvider>,
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
