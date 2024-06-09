a27b456a83bf229dad5a75b436ec86b38405b875
olimgulomov98 <olimgulomov98@gmail.com>
2024-06-09 17:41:48 +0500

    feat: create routing system and develop nexton screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { RippleBadge } from './MaterialTheme/styled';
+import { Link, Route, Switch } from 'react-router-dom';
+import { ShopPage } from './screens/shopPage';
+import { OrdersPage } from './screens/ordersPage';
+import { HelpPage } from './screens/helpPage';
+import { UserPage } from './screens/userPage';
+import { HomePage } from './screens/homePage';
 import '../css/app.css';
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Stack flexDirection={"column"}>
-        <Box sx={{my: 4}}>
-          <Typography variant='h4' component={"h4"}>
-            Create React App on Typescript with REDUX
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={4}>
-           <Button variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">HomePage</Link>
+        </li>
+        <li>
+          <Link to="/shop">ShopPage</Link>
+        </li>
+        <li>
+          <Link to="/orders">OrdersPage</Link>
+        </li>
+        <li>
+          <Link to="/help">HelpPage</Link>
+        </li>
+        <li>
+          <Link to="/member-page">UserPage</Link>
+        </li>
+      </ul>
+    </nav>
+
+    {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+    <Switch>
+      <Route path="/shop">
+        <ShopPage />
+      </Route>
+      <Route path="/orders">
+        <OrdersPage />
+      </Route>
+      <Route path="/help">
+        <HelpPage />
+      </Route>
+      <Route path="/member-page">
+        <UserPage />
+      </Route>
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </Switch>
+  </div>
   );
 }
 
---
+++ src/app/screens/shopPage/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export function ShopPage() {
+  return <Container>ShopPage</Container>;
+}
