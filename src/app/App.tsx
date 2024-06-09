e0a1951f6a4874b4db8445d5ba76ffa09cb339b4
olimgulomov98 <olimgulomov98@gmail.com>
2024-06-09 19:20:17 +0500

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,55 +1,40 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import { ShopPage } from './screens/shopPage';
 import { OrdersPage } from './screens/ordersPage';
 import { HelpPage } from './screens/helpPage';
 import { UserPage } from './screens/userPage';
 import { HomePage } from './screens/homePage';
+import { HomeNavbar } from './components/headers/HomeNavbar';
+import { OtherNavbar } from './components/headers/OtherNavbar';
+import { Footer } from './components/footer';
 import '../css/app.css';
 
 function App() {
+  const location = useLocation()
+  
   return (
-    <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">HomePage</Link>
-        </li>
-        <li>
-          <Link to="/shop">ShopPage</Link>
-        </li>
-        <li>
-          <Link to="/orders">OrdersPage</Link>
-        </li>
-        <li>
-          <Link to="/help">HelpPage</Link>
-        </li>
-        <li>
-          <Link to="/member-page">UserPage</Link>
-        </li>
-      </ul>
-    </nav>
-
-    {/* A <Switch> looks through its children <Route>s and
-        renders the first one that matches the current URL. */}
-    <Switch>
-      <Route path="/shop">
-        <ShopPage />
-      </Route>
-      <Route path="/orders">
-        <OrdersPage />
-      </Route>
-      <Route path="/help">
-        <HelpPage />
-      </Route>
-      <Route path="/member-page">
-        <UserPage />
-      </Route>
-      <Route path="/">
-        <HomePage />
-      </Route>
-    </Switch>
-  </div>
+   <>
+    {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        <Route path="/shop">
+          <ShopPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    <Footer />
+  </>
   );
 }
 
